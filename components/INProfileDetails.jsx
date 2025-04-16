b1183347f222885a2a73b18220c81faad66b04d4
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-04-16 05:03:03 +0200

    add personal social medias modal instructor profile

--- components/INProfileDetails.jsx
+++ components/INProfileDetails.jsx
@@ -1,17 +1,18 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Modal, FlatList, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 import { icons } from '../constants'
 import FormField from './FormField'
 import CustomButton from './CustomButton'
+import { Platform } from 'react-native'
 
 const INProfileDetails = ({user}) => {
     const [userData, setUserData] = useState(user)
     const [userSocials, setUserSocials] = useState(null)
     const [showDetails, setShowDetails] = useState(true)
+    const [socialsError, setSocialsError] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
-    console.log(userData);
-    
+    const [openSocialsModal, setOpenSocialsModal] = useState(false)
 
     useEffect(() => {
       if(user){
@@ -66,6 +67,7 @@ const INProfileDetails = ({user}) => {
                     title={"Emri i plote"}
                     placeholder={"Shkruani emrin e plote"}
                     value={userData?.name}
+                    handleChangeText={(e) => setUserData((prevData) => ({...prevData, name: e}))}
                 />
                 <Text className="text-xs text-gray-400 font-plight mt-1">Ky emer do paraqitet kudo ne aplikacion.</Text>
             </View>
@@ -74,6 +76,7 @@ const INProfileDetails = ({user}) => {
                     title={"Nofka juaj"}
                     placeholder={"Shkruani nofken tuaj"}
                     value={userData?.username}
+                    handleChangeText={(e) => setUserData((prevData) => ({...prevData, username: e}))}
                 />
                 <Text className="text-xs text-gray-400 font-plight mt-1">Ne baze te kesaj nofke ju mund te identifoheni me vone.</Text>
             </View>
@@ -82,6 +85,7 @@ const INProfileDetails = ({user}) => {
                     title={"Emaili juaj"}
                     placeholder={"Shkruani emailin tuaj"}
                     value={userData?.email}
+                    handleChangeText={(e) => setUserData((prevData) => ({...prevData, email: e}))}
                 />
                 <Text className="text-xs text-gray-400 font-plight mt-1">Emaili juaj elektronik personal. Verifikimi i veprimeve ne aplikacion behet permes ketij emaili.</Text>
             </View>
@@ -90,6 +94,7 @@ const INProfileDetails = ({user}) => {
                     title={"Numri juaj"}
                     placeholder={"Shkruani numrin tuaj"}
                     keyboardType="number-pad"
+                    
                 />
                 <Text className="text-xs text-gray-400 font-plight mt-1">Numri juaj personal. Verifikimi i veprimeve dhe kontaktet ne aplikacion behen permes ketij numri.</Text>
             </View>
@@ -108,6 +113,7 @@ const INProfileDetails = ({user}) => {
                     title={"Ekspertiza"}
                     placeholder={"Shkruani ekspertizen tuaj ketu"}
                     value={userData?.expertise}
+                    handleChangeText={(e) => setUserData((prevData) => ({...prevData, expertise: e}))}
                 />
                 <Text className="text-xs text-gray-400 font-plight mt-1">Kjo ekspertize shfaqet ne profil/kurse.</Text>
             </View>
@@ -117,13 +123,14 @@ const INProfileDetails = ({user}) => {
                     placeholder={"Shkruani pjese nga jeta juaj profesionale"}
                     multiline
                     value={userData?.bio}
+                    handleChangeText={(e) => setUserData((prevData) => ({...prevData, bio: e}))}
                 />
                 <Text className="text-xs text-gray-400 font-plight mt-1">Pjesa e biografise mund te terheqe me shume studente. Shfaqet poashtu ne profil/kurse.</Text>
             </View>
             <View>
                 <View className="flex-row items-center justify-between">
                     <Text className="text-base text-gray-100 font-pmedium mb-1">Rrjetet sociale</Text>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => setOpenSocialsModal(true)}>
                         <Text className="text-secondary font-psemibold text-xs underline">Beje ndryshime</Text>
                     </TouchableOpacity>
                 </View>
@@ -171,44 +178,173 @@ const INProfileDetails = ({user}) => {
         </View>
     )}
 
+    <Modal
+        visible={openSocialsModal}
+        transparent
+        animationType='slide'
+        onRequestClose={() => setOpenSocialsModal(false)}
+    >
+        <View className="flex-1 justify-center items-center" style={{ backgroundColor: "rgba(0,0,0,0.4)" }}>
+            <View className="h-[90%] w-[90%] bg-oBlack rounded-[10px] border border-black-200 justify-between" style={styles.box}>
+                <View className="border-b border-black-200 flex-1">
+                    <FlatList 
+                        contentContainerStyle={{gap:24, paddingLeft: 16, paddingRight: 16}}
+                        data={userSocials}
+                        keyExtractor={(item) => item.Label}
+                        ListHeaderComponent={() => (
+                            <View className="mx-auto my-4 border-b border-black-200 bg-oBlack rounded-b-[10px] -mb-2" style={styles.box}>
+                                <Text className="text-white font-psemibold text-2xl text-center border-b border-secondary self-start">Rrjete sociale</Text>
+                            </View>
+                        )}
+                        renderItem={({item, index}) => {
+                            const isValidUrl = (url) => {
+                                    const pattern = new RegExp(
+                                    "^(https?:\\/\\/)?" +                             // optional protocol
+                                    "((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+" + // domain name
+                                    "[a-zA-Z]{2,}|" +                                  // domain extension
+                                    "((\\d{1,3}\\.){3}\\d{1,3}))" +                    // OR IP address
+                                    "(\\:\\d+)?(\\/[-a-zA-Z\\d%@_.~+&:]*)*" +          // optional port and path
+                                    "(\\?[;&a-zA-Z\\d%@_.,~+&:=-]*)?" +                // optional query
+                                    "(\\#[-a-zA-Z\\d_]*)?$",                          // optional fragment
+                                    "i"
+                                    );
+                                    return pattern.test(url);
+                                };
+
+                                const getSocialIcon = (link) => {
+                                    if (!link) return {icon: icons.info, label: "Other"};
+                                
+                                    const lowercase = link.toLowerCase();
+                                
+                                    if (lowercase.includes("github")) return {icon: icons.githubIcon, label: "Github"};                           // GitHub
+                                    if (lowercase.includes("meta") || lowercase.includes("facebook")) return {icon: icons.metaIcon, label: "Facebook"}; // Facebook
+                                    if (lowercase.includes("insta") || lowercase.includes("instagram")) return {icon: icons.instagramIcon, label: "Instagram"}; // Instagram
+                                    if (lowercase.includes("twitter") || lowercase.includes("x.com")) return {icon: icons.twitterIcon, label: "Twitter"};  // Twitter/X
+                                    if (lowercase.includes("linkedin")) return {icon: icons.linkedinIcon, label: "Linkedin"};                      // LinkedIn
+                                    if (lowercase.includes("tiktok")) return {icon: icons.tiktokIcon, label: "Tiktok"};                          // TikTok
+                                    if (lowercase.includes("youtube") || lowercase.includes("youtu.be")) return {icon: icons.youtubeIcon, label: "Youtube"}; // YouTube
+                                    if (lowercase.includes("reddit")) return {icon: icons.redditIcon, label: "Reddit"};                          // Reddit
+                                    if (lowercase.includes("snapchat")) return {icon: icons.snapchatIcon, label: "Snapchat"};                      // Snapchat
+                                    if (lowercase.includes("discord")) return {icon: icons.discordIcon, label: "Discord"};                        // Discord
+                                    if (lowercase.includes("t.me") || lowercase.includes("telegram")) return {icon: icons.telegramIcon, label: "Telegram"}; // Telegram
+                                
+                                    return {icon: icons.info, label: "Other"}; // Fallback icon if no match found
+                                };
+
+                                const getSpecifiedIcon = () => {
+                                    if(item?.Link){
+
+                                        const lowercase = item?.Link?.toLowerCase();
+                                        
+                                        if (lowercase.includes("github")) return {icon: icons.githubIcon, label: "Github"};                           // GitHub
+                                        if (lowercase.includes("meta") || lowercase.includes("facebook")) return {icon: icons.metaIcon, label: "Facebook"}; // Facebook
+                                        if (lowercase.includes("insta") || lowercase.includes("instagram")) return {icon: icons.instagramIcon, label: "Instagram"}; // Instagram
+                                        if (lowercase.includes("twitter") || lowercase.includes("x.com")) return {icon: icons.twitterIcon, label: "Twitter"};  // Twitter/X
+                                        if (lowercase.includes("linkedin")) return {icon: icons.linkedinIcon, label: "Linkedin"};                      // LinkedIn
+                                        if (lowercase.includes("tiktok")) return {icon: icons.tiktokIcon, label: "Tiktok"};                          // TikTok
+                                        if (lowercase.includes("youtube") || lowercase.includes("youtu.be")) return {icon: icons.youtubeIcon, label: "Youtube"}; // YouTube
+                                        if (lowercase.includes("reddit")) return {icon: icons.redditIcon, label: "Reddit"};                          // Reddit
+                                        if (lowercase.includes("snapchat")) return {icon: icons.snapchatIcon, label: "Snapchat"};                      // Snapchat
+                                        if (lowercase.includes("discord")) return {icon: icons.discordIcon, label: "Discord"};                        // Discord
+                                        if (lowercase.includes("t.me") || lowercase.includes("telegram")) return {icon: icons.telegramIcon, label: "Telegram"}; // Telegram
+                                    }
+                                }
+                                
+                                const updateSocialsData = (idx, link) => {
+                                    const isValid = isValidUrl(link);
+                                    const icon = isValid ? getSocialIcon(link) : icons.info;
+                                    setUserSocials(prev => {
+                                        const newData = [...prev];
+                                        newData[idx] = { link, icon: icon.icon, label: icon.label };
+                                        return newData;
+                                    });
+
+                                    setSocialsError(prev => {
+                                        const newErrors = [...prev];
+                                        newErrors[idx] = isValid ? null : "Linku nuk është valid!";
+                                        return newErrors;
+                                    });
+                                }
+
+                                const removeSocial = (idx) => {
+                                    setUserSocials((prevData) => {
+                                        return prevData.filter((_, index) => index !== idx)
+                                    })
+                                }
+                            return (
+                                <View className="relative">
+                                    {index > 0 && <TouchableOpacity onPress={() => removeSocial(index)} className="bg-oBlack absolute rounded-xl items-center justify-center border-2 border-black-200 h-8 w-8 -left-4 top-6 z-20">
+                                        <Image 
+                                            source={icons.close}
+                                            className="h-8 w-8 p-2"
+                                            resizeMode='contain'
+                                            tintColor={"#ff9c01"}
+                                        />
+                                    </TouchableOpacity>}
+                                    {index === userSocials.length - 1 &&<TouchableOpacity 
+                                        className="bg-oBlack rounded-xl items-center justify-center border-2 border-black-200 h-10 w-10 absolute right-0 -top-4"
+                                        onPress={() => setUserSocials((prevData) => [...prevData, {}])}
+                                        >
+                                        <Image
+                                            source={icons.plus}
+                                            className="h-8 w-8 p-1"
+                                            resizeMode='contain'
+                                            tintColor={"#ff9c01"}
+                                        />
+                                    </TouchableOpacity>}
+                                    <View className="flex-row gap-1.5">
+                                        <View className="flex-1">
+                                            <FormField
+                                                title={`Rrjeti social ${index + 1}`}
+                                                placeholder={"P.sh linku Meta, Github etj."}
+                                                value={userSocials[index]?.Link}
+                                                handleChangeText={(e) => updateSocialsData(index, e)}
+                                            />
+                                        </View>
+                                        <View className="flex-[0.2] justify-end relative">
+                                            <View className="border-2 items-center justify-center border-black-200 rounded-xl h-16 bg-oBlack">
+                                                <Image 
+                                                    source={userSocials[index]?.icon ?? getSpecifiedIcon()?.icon}
+                                                    className="h-10 w-10"
+                                                    tintColor={"#ff9c01"}
+                                                />
+                                            </View>
+                                        </View>
+                                    </View>
+                                    <Text className="text-gray-400 text-xs mt-1 font-plight">Ne paraqitje te linkut do shfaqet ikona e duhur.</Text>
+                                    {socialsError[index] && <Text className="font-plight text-red-500 mt-1 text-xs">{socialsError[index]}</Text>}
+                                </View>
+                            )
+                        }}
+                    />
+                </View>
+                <View className="h-[60px]">
+                    <TouchableOpacity className="bg-oBlack border-t items-center justify-center flex-1 border-black-200" onPress={() => setOpenSocialsModal(false)} style={styles.box}>
+                        <Text className="text-sm font-psemibold text-white">Largoni dritaren</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    </Modal>
     </>
   )
 }
 
 export default INProfileDetails
 
-const dummyData = [
-    {
-      "link": "https://www.facebook.com/yourprofile",
-      "icon": "Facebook"
-    },
-    {
-      "link": "https://www.instagram.com/yourprofile",
-      "icon": "Instagram"
-    },
-    {
-      "link": "https://www.linkedin.com/in/yourprofile",
-      "icon": "LinkedIn"
-    },
-    {
-      "link": "https://www.twitter.com/yourprofile",
-      "icon": "Twitter"
-    },
-    {
-      "link": "https://www.youtube.com/c/yourchannel",
-      "icon": "YouTube"
-    },
-    {
-      "link": "https://www.tiktok.com/@yourprofile",
-      "icon": "TikTok"
-    },
-    {
-      "link": "https://www.github.com/yourprofile",
-      "icon": "GitHub"
-    },
-    {
-      "link": "https://yourwebsite.com",
-      "icon": "Website"
-    }
-  ]
+const styles = StyleSheet.create({
+    box: {
+      ...Platform.select({
+          ios: {
+              shadowColor: '#000',
+              shadowOffset: { width: 0, height: 4 },
+              shadowOpacity: 0.6,
+              shadowRadius: 10,
+            },
+            android: {
+              elevation: 8,
+            },
+      })
+  },
+  });
   
