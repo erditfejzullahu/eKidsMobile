2eaeed57c69e17c2c358af607f81db82ffc8a286
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-05-16 01:45:14 +0200

    fix in webview allowned of live videos playback

--- components/FullscreenWebViewModal.jsx
+++ components/FullscreenWebViewModal.jsx
@@ -66,21 +66,33 @@ const FullscreenWebViewModal = ({ visible, onClose, url }) => {
           ref={webViewRef}
           source={{ 
             uri: url,
-            headers: {
-              // Cookie: 'your_cookie_key=your_cookie_value' // Set your cookies here
-            }
           }}
+          scalesPageToFit={true}
+          useWebView2={true}
+          allowsInlineMediaPlayback={true}
             style={styles.webview}
             sharedCookiesEnabled={true} // Shares cookies with the device's browser
             thirdPartyCookiesEnabled={true} // Allows third-party cookies (mostly for Android)
             javaScriptEnabled={true}
+            javaScriptCanOpenWindowsAutomatically={true}
             domStorageEnabled={true} // Required for localStorage/sessionStorage
             startInLoadingState={true}
             allowsBackForwardNavigationGestures={true} // iOS swipe navigation
             // Android-specific optimizations
             androidLayerType="hardware" // Better rendering performance
             androidHardwareAccelerationDisabled={false} // Enable hardware acceleration
+            allowFileAccess={true}
+            mediaCapturePermissionGrantType="grant"
+            mediaPlaybackRequiresUserAction={false}
+            onError={(s) => console.log("WebView Error:", s.nativeEvent)}
+            allowUniversalAccessFromFileURLs={true}
+            allowFileAccessFromFileURLs={true}
+            allowsAirPlayForMediaPlayback={true}
+            allowsFullscreenVideo={true}
+            webviewDebuggingEnabled={true}
+            lackPermissionToDownloadMessage='testtest'
             mixedContentMode="always" // Allow mixed content (HTTP/HTTPS)
+            originWhitelist={['*']}
             // onMessage={(event) => {
             //   // Handle messages from the web page
             //   if (event.nativeEvent.data === 'CLOSE_MODAL') {
@@ -102,11 +114,10 @@ const FullscreenWebViewModal = ({ visible, onClose, url }) => {
                 accessToken: tokens.accessToken,
                 refreshToken: tokens.refreshToken
               })
-
+              console.log('Message being sent:', message);
               webViewRef.current.injectJavaScript(`
-                window.dispatchEvent(new MessageEvent('message', {
-                  data: ${JSON.stringify(message)}
-                }));
+                console.log('About to post message from WebView');
+                window.postMessage(${JSON.stringify(message)}, '*');
                 true;
                 `)
             }}
